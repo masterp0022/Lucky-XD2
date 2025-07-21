8106833039387679da1291f2c545a88a78e007b1
masterp0022 <112967412+masterp0022@users.noreply.github.com>
2025-07-21 15:20:09 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -68,7 +68,7 @@ LINK_WHITELIST: "youtube.com,github.com",
           DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
 
-          OWNER_NUMBER: process.env.OWNER_NUMBER || "2347048830366",
+          OWNER_NUMBER: process.env.OWNER_NUMBER || "2349074389186",
 // add your bot owner number
 
 OWNER_NAME: process.env.OWNER_NAME || "MP",
